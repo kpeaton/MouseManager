2d9a0f021bddf1a9f00f591e0fce7e5b51a669c9
EATYO5 <EATYO5@P17-1775.chmccorp.cchmc.org>
2017-02-28 00:56:30 -0500

    Updated help and added empty Contents.m.

--- MouseManager.m
+++ MouseManager.m
@@ -1,24 +1,34 @@
 classdef MouseManager < handle
 %MouseManager   Class object for managing interactive mouse-based controls.
-%   The MouseManager class provides a general-purpose interface for
-%   managing mouse-based interactions with figure objects.
+%   MMOBJ = MouseManager(HFIGURE) will create a MouseManager object MMOBJ
+%   that provides a general-purpose interface for managing mouse-based
+%   interactions with figure objects. HFIGURE must be a valid figure
+%   handle. The lifecycle of MMOBJ is bound to HFIGURE; deleting HFIGURE
+%   will cause MMOBJ to be deleted as well.
 %
-%   See also handle
+%   Graphics objects to be managed by MMOBJ, along with their pertinent
+%   callback functions, can be added using the MouseManager.add_item
+%   method. MMOBJ can be enabled/disabled using the MouseManager.enable
+%   method.
+%
+%   See also handle, MouseManager.enable, MouseManager.add_item,
+%            MouseManager.default_hover_fcn.
 
 % TODO:
 % - Do HandleVisibility and HitTest properties of objects need to be
 %   modified when added?
 % - Allow input of just the selection type without the operation!
-% - Is the use of drawnow limitrate OK?!
-% - drawnow is not advised to use in user functions!
 % - Add listeners for object deletions and WindowFcn changes!
+% - Add Tag property?
+% - Add check to enable() for numeric/logical input.
 % - Add help!
 %   - Fix help for public methods!
 % - Add disp method to show table of function handles?
+% - Will mouse_op still work if private method?
 
 % Author: Ken Eaton
 % Version: MATLAB R2016b
-% Last modified: 2/26/17
+% Last modified: 2/28/17
 % Copyright 2017 by Kenneth P. Eaton
 %--------------------------------------------------------------------------
 
@@ -26,33 +36,32 @@ classdef MouseManager < handle
 
   %------------------------------------------------------------------------
   properties (SetAccess = immutable)
-  %   Properties that need to be defined on construction.
-  %------------------------------------------------------------------------
+
     hFigure
+
   end
 
   %------------------------------------------------------------------------
   properties (SetAccess = private)
-  %   Properties that are modified by class methods only.
-  %------------------------------------------------------------------------
+
     enabled logical = false
+    itemList
+    isHoverable
+    itemFcnTable
+    defaultHoverFcn
+
   end
 
   %------------------------------------------------------------------------
   properties (Access = private)
-  %   Private properties.
-  %------------------------------------------------------------------------
+
     isActive logical = false
     selectionType = 'none'
     figurePoint
     itemIndex
     hoverRegion
     scrollEventData
-    % Move below to property section above?
-    itemList
-    isHoverable
-    itemFcnTable
-    defaultHoverFcn
+
   end
 
 %~~~Event blocks~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -61,13 +70,9 @@ classdef MouseManager < handle
 
   %------------------------------------------------------------------------
   methods
-  %   Public methods.
-  %------------------------------------------------------------------------
 
     %----------------------------------------------------------------------
     function obj = MouseManager(hFigure)
-    %   MouseManager constructor.
-    %----------------------------------------------------------------------
 
       if (nargin > 0)
         assert(ishandle(hFigure) && strcmpi(hFigure.Type, 'figure'), ...
@@ -81,8 +86,18 @@ classdef MouseManager < handle
 
     %----------------------------------------------------------------------
     function enable(obj, newState)
-    %   Function for enabling/disabling mouse control.
-    %----------------------------------------------------------------------
+    %MouseManager.enable   Enable/disable a MouseManager object.
+    %   MMOBJ.enable(NEWSTATE) will set the new enabled state of MMOBJ to
+    %   NEWSTATE. NEWSTATE can be a boolean (TRUE/FALSE) or character
+    %   string ('on'/'off') input.
+    %
+    %   When enabled, the WindowButtonDownFcn, WindowButtonMotionFcn,
+    %   WindowButtonUpFcn, and WindowScrollWheelFcn properties of the
+    %   linked figure will be updated for use by MMOBJ. If at any time
+    %   these properties are modified, all of them will be removed and
+    %   MMOBJ will be disabled.
+    %
+    %   See also MouseManager.
 
       % Convert character string input into a logical:
 
@@ -118,8 +133,44 @@ classdef MouseManager < handle
 
     %----------------------------------------------------------------------
     function add_item(obj, hItem, varargin)
-    %   Function for adding interactive control for a graphics object.
-    %----------------------------------------------------------------------
+    %MouseManager.add_item   Add interactive mouse controls for an object.
+    %   MMOBJ.add_item(H, (OPER), (SELECTION), CALLBACKFCN, ...) will add a
+    %   graphics object H to a MouseManager object MMOBJ such that the
+    %   function CALLBACKFCN will be invoked for a given mouse operation
+    %   OPER and mouse selection SELECTION.
+    %
+    %   OPER can be any one of: 'click', 'drag', 'release', 'hover', or
+    %   'scroll'. SELECTION can be any one of: 'normal' (left click),
+    %   'extend' (middle click), 'alt' (right click), or 'open' (double
+    %   click). One or both of OPER or SELECTION can be a cell array
+    %   containing a subset of the above values, in which case all
+    %   combinations of OPER and SELECTION will invoke CALLBACKFCN. One or
+    %   both of OPER or SELECTION can be omitted, in which case all
+    %   possible respective values will be used.
+    %
+    %   CALLBACKFCN must be written to accept two input arguments: a handle
+    %   SOURCE and a structure EVENTDATA. SOURCE will be the handle H, and
+    %   EVENTDATA will be a structure with the following fields:
+    %       operation        -- Mouse operation (values above)
+    %       selectionType    -- Mouse selection (values above, or 'none')
+    %       figurePoint      -- The CurrentPoint property of the linked
+    %                           figure when CALLBACKFCN is invoked
+    %       figureRegion     -- The position of H in pixels relative to the
+    %                           linked figure (from getpixelposition)
+    %       scrollEventData  -- Event data for scroll operations (empty
+    %                           when not scrolling)
+    %   You should not have to make any calls to drawnow within
+    %   CALLBACKFCN, as graphics refreshing is handled by the MouseManager
+    %   class object.
+    %
+    %   The input argument list can contain repeated sets of OPER,
+    %   SELECTION, and CALLBACKFCN for setting more than one callback
+    %   function for object H in a single call.
+    %
+    %   If H is deleted at any time, all callback functions associated with
+    %   H through add_item will be removed from MMOBJ.
+    %
+    %   See also MouseManager, getpixelposition.
 
       % Add the new graphics object if it is not in the list already:
 
@@ -164,9 +215,14 @@ classdef MouseManager < handle
 
     %----------------------------------------------------------------------
     function default_hover_fcn(obj, hoverFcn)
-    %   Function for adding a default hover function (evaluates when the
-    %   mouse is not hovering over any hoverable items).
-    %----------------------------------------------------------------------
+    %MouseManager.default_hover_fcn   Add a default hover function.
+    %   MMOBJ.default_hover_fcn(@CALLBACKFCN) will add a callback function
+    %   CALLBACKFCN to a MouseManager object MMOBJ to be evaluated when the
+    %   mouse is hovering over the parent figure window but not over any
+    %   other object managed by MMOBJ which has a callback defined for
+    %   hovering or scrolling behavior.
+    %
+    %   See also MouseManager, MouseManager.add_item.
 
       if ~isempty(hoverFcn)
         assert(isa(hoverFcn, 'function_handle'), ...
@@ -178,9 +234,8 @@ classdef MouseManager < handle
     end
 
     %----------------------------------------------------------------------
+    % Evaluate mouse operations.
     function mouse_op(obj, ~, eventData, mouseOperation)
-    %   Function that evaluates mouse operations.
-    %----------------------------------------------------------------------
 
       switch mouseOperation
 
@@ -240,13 +295,10 @@ classdef MouseManager < handle
 
   %------------------------------------------------------------------------
   methods (Access = private)
-  %   Private methods.
-  %------------------------------------------------------------------------
 
     %----------------------------------------------------------------------
+    % Check if an item was last selected by clicking.
     function clickSelected = click_selected(obj)
-    %   Function for checking if an item was last selected by click.
-    %----------------------------------------------------------------------
 
       obj.itemIndex = [];
       obj.hoverRegion = [];
@@ -258,9 +310,8 @@ classdef MouseManager < handle
     end
 
     %----------------------------------------------------------------------
+    % Check if an item was last selected by hovering.
     function hoverSelected = hover_selected(obj)
-    %   Function for checking if an item was last selected by hovering.
-    %----------------------------------------------------------------------
 
       obj.itemIndex = [];
       obj.hoverRegion = [];
@@ -279,9 +330,8 @@ classdef MouseManager < handle
     end
 
     %----------------------------------------------------------------------
+    % Fetch and evaluate a mouse operation.
     function evaluate_operation(obj, oper)
-    %   Function for fetching and evaluating a mouse operation.
-    %----------------------------------------------------------------------
 
       if ~isempty(obj.itemIndex)
         fcn = obj.itemFcnTable(obj.itemIndex).(oper);
@@ -298,9 +348,8 @@ classdef MouseManager < handle
     end
 
     %----------------------------------------------------------------------
+    % Create an event data structure.
     function eventData = event_data(obj, oper)
-    %   Function to create an event data structure.
-    %----------------------------------------------------------------------
 
       eventData = struct('operation', oper, ...
                          'selectionType', obj.selectionType, ...
@@ -314,13 +363,10 @@ classdef MouseManager < handle
 
   %------------------------------------------------------------------------
   methods (Access = private, Static)
-  %   Static helper functions.
-  %------------------------------------------------------------------------
 
     %----------------------------------------------------------------------
+    % Create a new entry for itemFcnTable.
     function newEntry = fcn_table_entry
-    %   Function to create a new entry for itemFcnTable.
-    %----------------------------------------------------------------------
 
       selectionStruct = struct('normal', [], ...
                                'extend', [], ...
@@ -336,9 +382,8 @@ classdef MouseManager < handle
     end
 
     %----------------------------------------------------------------------
+    % Parse input arguments.
     function [argList, inArgs] = parse_input(argList, inArgs)
-    %   Function to parse input arguments.
-    %----------------------------------------------------------------------
 
       % Check up to 3 arguments from the argument list:
 
